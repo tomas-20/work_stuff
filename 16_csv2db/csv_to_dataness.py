709bbe92f8c187dae89cc70963e71f0f7302a471
tomas-20 <tacuna20@stuy.edu>
2021-10-22 11:00:56 -0400

    copied Ishraq's folder for 16_csv2db

--- 16_csv2db/csv_to_dataness.py
+++
@@ -1,7 +0,0 @@
-from csv import DictReader
-from sqlite3 import connect
-def copy_data(table_name, data_name, file_name):
-    with open(file_name) as file, connect(data_name) as data:
-        reader = file.DictReader()
-        cursor = data.cursor()
-        cursor.execute("CREATE TABLE " + table_name + " (" + 
