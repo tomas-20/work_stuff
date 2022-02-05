847807397f1908e6739629d06d8d21d9b358679b
tomas-20 <tacuna20@stuy.edu>
2022-02-05 18:18:56 -0500

    added fancy testing to fibfac.scm

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -8,8 +8,14 @@
   (if (zero? n)
       a
       (fib-helper (sub1 n) b (+ a b))))
+
 (define (fib n)
   (fib-helper n 0 1))
 
-(fac 20)
-(fib 20)
+;heretic imperative programming:
+(begin
+  (printf "enter number to fibfac: ")
+  (let ([n (string->number (read-line))])
+    (begin
+      (printf "fib of ~a is ~a\n" n (fib n))
+      (printf "fac of ~a is ~a\n" n (fac n)))))
