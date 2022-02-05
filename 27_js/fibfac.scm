4af0d3ecee04b207fbbadccd61e9fd526bbac4bf
tomas-20 <tacuna20@stuy.edu>
2022-02-05 17:27:28 -0500

    added a fib function to fibfac.scm

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -4,4 +4,12 @@
       1
       (* n (fac (sub1 n)))))
 
+(define (fib-helper n a b)
+  (if (zero? n)
+      a
+      (fib-helper (sub1 n) b (+ a b))))
+(define (fib n)
+  (fib-helper n 0 1))
+
 (fac 20)
+(fib 20)
