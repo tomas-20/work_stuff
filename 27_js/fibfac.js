8542acdeb54dd0600de3d0f48622f04bbed08d07
tomas-20 <tacuna20@stuy.edu>
2022-02-05 17:43:58 -0500

    added a fib function to fibfac.js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -6,3 +6,16 @@ function fac(n) {
     return n * fac(n - 1);
   }
 }
+
+function fibHelper(n, a, b) {
+  if (n === 0) {
+    return a;
+  }
+  else {
+    return fibHelper(n - 1, b, a + b);
+  }
+}
+
+function fib(n) {
+  return fibHelper(n, 0, 1);
+}
