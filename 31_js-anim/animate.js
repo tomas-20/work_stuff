db312f2c39d9290e23802d4944b610e7a84936b9
tomas-20 <tacuna20@stuy.edu>
2022-02-17 09:08:34 -0500

    added stuff to animate.js

--- 31_js-anim/animate.js
+++ 31_js-anim/animate.js
@@ -1,1 +1,15 @@
 var ctx = slate.getContext("2d");
+
+var requestId;
+
+function clear() {
+  ctx.clearRect(0, 0, slate.width, slate.height);
+}
+
+var radius = 0;
+var growing = true;
+
+function drawDot() {
+  clear();
+
+}
